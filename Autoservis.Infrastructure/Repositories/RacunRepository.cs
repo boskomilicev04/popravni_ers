ff350dbafa72836e3956424bd4c49b1c5a2ebfd2
boskomilicev04 <boskomilicev04@gmail.com>
2026-03-14 19:03:00 +0100

    Infrastructure

---
+++ Autoservis.Infrastructure/Repositories/RacunRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autoservis.Domain;
+using System.Text.Json;
+
+namespace Autoservis.Infrastructure.Repositories
+{
+    public class RacunRepository
+    {
+        private readonly string _putanja = "racuni.json";
+
+        public List<Racun> GetAll()
+        {
+            if(!File.Exists(_putanja)) return new List<Racun>();
+            string json = File.ReadAllText(_putanja);
+            return JsonSerializer.Deserialize<List<Racun>>(json) ?? new List<Racun>();
+        }
+
+        public void SaveAll(List<Racun> racuni)
+        {
+            var opcije = new JsonSerializerOptions { WriteIndented = true };
+            string json = JsonSerializer.Serialize(racuni, opcije);
+            File.WriteAllText(_putanja, json);
+        }
+    }
+}
