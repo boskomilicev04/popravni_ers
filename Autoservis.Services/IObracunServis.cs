411964c5912c8b1a450641e6ee1c5bd690edb0c5
boskomilicev04 <boskomilicev04@gmail.com>
2026-03-14 19:04:43 +0100

    Services

---
+++ Autoservis.Services/IObracunServis.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Autoservis.Services
+{
+    public interface IObracunServis
+    {
+        decimal IzracunajCenu(decimal osnovnaCena);
+    }
+}
